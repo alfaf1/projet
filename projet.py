4c2ab46dbea33795731bf8f8a13c96d3488dfcce
alfaf1 <frederic.turchet@gmail.com>
2019-04-29 20:54:35 +0200

    projet.py avec des routes tests

--- projet.py
+++ projet.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'a39938273acbdcd3fbd1e96a1b9efb36'
 
 #Initialisation de la base de donnees
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db_setup import Base, User, Tournaments
+
 
 engine = create_engine('sqlite:///tournament.db')
 Base.metadata.bind = engine
@@ -41,6 +44,30 @@ def About():
 @app.route("/navigation")
 def Navigation():
     return render_template('navigation.html', title='Navigation')
+
+@app.route("/nav")
+def Nav():
+    return render_template('nav.html', title='Navigation')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account created for {form.username.data}!', 'success')
+		return redirect(url_for('home'))
+	return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+			flash('You have been logged in!', 'success')
+			return redirect(url_for('home'))
+		else:
+			flash('Login Unsuccessful. Please check your username and your password', 'danger')
+	return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
 	app.debug = True
